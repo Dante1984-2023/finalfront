a98c8e67c4fe4d299792bab69cfc4741bca1a345
Dante <galmesdante@hotmail.com>
2023-07-25 00:16:07 -0300

    corrijo API

--- src/routes/Signup.tsx
+++ src/routes/Signup.tsx
@@ -19,7 +19,7 @@ export default function Signup() {
     console.log(username, password, name);
 
     try {
-      const response = await fetch( `${API_URL}/api/signup`, {
+      const response = await fetch( `${API_URL}/signup`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password, name }),
